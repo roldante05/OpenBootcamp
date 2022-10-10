b9f26a5dc46b2124a75852a71cecab61cfa872bb
Dante Roldan <danteroldan72@gmail.com>
2022-10-09 23:56:00 -0300

    componente de tipo clase

--- React/hola-mundo/src/App.js
+++ React/hola-mundo/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './componentes/pure/greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo con React
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/*Componente propio de greeting */}
+
+      <Greeting name= "Dante"/>
         
       </header>
     </div>
---
+++ React/hola-mundo/src/componentes/pure/greeting.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greeting extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         age: 21
+      }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1> Edad {this.state.age} </h1>
+                <h1> Hola {this.props.name} </h1>
+                <div>
+                <button onClick={this.birthday} >Cumplir</button>
+                </div>
+            </div>
+        );
+    }
+    birthday = () => {
+       this.setState((prevState) => (
+           {
+               age: prevState.age +1
+           }
+       ))
+    }
+}
+ 
+
+Greeting.propTypes = {
+
+};
+
+
+export default Greeting;
